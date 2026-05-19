d1535850ec5dc751abeb7b501003f8431faa9307
petty-thief_4_0 <2810739917@qq.com>
2026-05-19 12:41:12 +0800

    更新

--- 电调/AM32_AI8051U-main/Inc/nstdbool.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_NSTDBOOL_H_
-#define INC_NSTDBOOL_H_
-
-typedef bit bool;
-
-#define true 1
-#define false 0
-
-#endif // !INC_NSTDBOOL_H_
--- 电调/AM32_AI8051U-main/docs/pcb/lceda/README.md
+++
@@ -1,9 +0,0 @@
-# LCEDA PCB
-* [PCB](https://oshwhub.com/lcet7122/stc-am32-firmware-porting)<br>
-
-
-
-
-
-
-
